66ef98b7b8e852bfa0796eddecd37f10e5a72639
mook <keettamook_k@cmu.ac.th>
2020-02-11 16:29:59 +0700

    mada

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -11,8 +11,40 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
+	int i,key,j;
+	for (i=1;i < N;i++){
+		int l,p,k,z,h;
+		for(l=0; l < i; l++){ 
+				cout << d[l] << " ";
+				p=l+2;
+			}
+			cout << "["<<d[i]<<"] ";
+		for(p; p<10;p++){ 
+				cout << d[p] << " ";
+			}
+			cout << "=> ";
+			
+		//main
+		for(j=i;j>0;j--){
+			if(d[j]> d[j-1]){
+				swap(d,j,j-1);
+			}
+		}
+		//end
+		for(k=0; k < i; k++){ 
+				cout << d[k] << " ";
+				z=k+2;
+			}
+		cout << "["<<d[]<<"] ";
+		for(z; z<10;z++){ 
+				cout << d[z] << " ";
+			}
+
+
+		cout << "\n";
 	
-//Write your code here.
+	}
+
 
 }
 
