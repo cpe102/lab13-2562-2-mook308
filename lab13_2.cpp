90b399fb9ca73e620101b8453b3495fe6208544c
mook <keettamook_k@cmu.ac.th>
2020-02-11 17:22:31 +0700

    ez 2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -37,10 +37,10 @@ void findLocalMax(const double A[][N], bool B[][N]){
 	}
 	for(int i=1;i<N-1;i++){
 		for(int j=1;j<N-1;j++){
-		if(A[i][j] > A[i][j+1]){
-			if(A[i][j] > A[i][j-1]){
-				if(A[i][j] > A[i+1][j]){
-					if(A[i][j] > A[i-1][j]){
+		if(A[i][j] >= A[i][j+1]){
+			if(A[i][j] >= A[i][j-1]){
+				if(A[i][j] >= A[i+1][j]){
+					if(A[i][j] >= A[i-1][j]){
 						B[i][j]=1;
 					}
 				}
